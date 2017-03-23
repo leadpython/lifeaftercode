8ed1255c95f466ce774ae2415d7757715b2ce73e
leadpython <jeffrey.campecino@gmail.com>
2017-03-22 18:40:35 -0700

    added other pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { PortfolioPageComponent } from './pages/portfolio-page/portfolio-page.component';
 import { HeaderNavComponent } from './components/header-nav/header-nav.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ResumePageComponent } from './pages/resume-page/resume-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
+  { path: 'home', component: HomePageComponent },
+  { path: 'resume', component: ResumePageComponent },
   { path: 'portfolio', component: PortfolioPageComponent },
+  { path: 'contact', component: ContactPageComponent },
   // { path: 'hero/:id',      component: HeroDetailComponent },
   // {
   //   path: 'heroes',
@@ -28,6 +35,10 @@ const appRoutes: Routes = [
     AppComponent,
     PortfolioPageComponent,
     HeaderNavComponent,
+    HomePageComponent,
+    ResumePageComponent,
+    ContactPageComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
