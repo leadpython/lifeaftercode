5701132e05b3b32dbfd4f9633bb061ea7283f5dd
leadpython <jeffrey.campecino@gmail.com>
2017-03-24 04:48:56 -0700

    express server file working

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ var express = require('express');
 var path = require("path");
 var app = express();
 
-app.use('/static', express.static(path.join(__dirname, 'client')))
+app.use(express.static(path.join(__dirname, 'dist')))
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/dist/index.html');
+  res.sendFile(__dirname + 'dist/index.html');
 });
 
 app.listen(process.env.PORT || 3000, function() {
-  console.log('listening on port 3000');
+  console.log('listening');
 });
