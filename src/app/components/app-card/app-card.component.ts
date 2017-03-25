81df4b51a65ddd46ce28f40fa416ab32ddbfa919
leadpython <jeffrey.campecino@gmail.com>
2017-03-25 11:37:02 -0700

    app cards rendered

--- src/app/components/app-card/app-card.component.ts
+++ src/app/components/app-card/app-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app-card.component.css']
 })
 export class AppCardComponent implements OnInit {
+  @Input() app;
 
   constructor() { }
 
